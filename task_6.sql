233b708a0a5edcfbdbfd64703f24ccb758d75e12
Kiruthims <Kiruthimiss@proton.me>
2025-02-01 12:16:57 -0800

    customers and quotes?

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,6 @@
--- Insert data into the 'customer' table (not 'Customers')
 INSERT INTO customer (customer_name, email, address)
-VALUES 
-    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    ('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES
+("Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+("Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+("Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.");
 
