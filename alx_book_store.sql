ec8cad1d7b8c43bbbc40621be3fc331f95a8e0c9
Kiruthims <Kiruthimiss@proton.me>
2025-02-01 07:47:57 -0800

    Updated alx_book_store.sql to include IF NOT EXISTS for database and tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,13 +1,17 @@
-CREATE DATABASE alx_book_store;
+-- Check if the database exists, if not, create it
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
+-- Use the database
 USE alx_book_store;
 
-CREATE TABLE authors (
+-- Create the 'authors' table
+CREATE TABLE IF NOT EXISTS authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE books (
+-- Create the 'books' table
+CREATE TABLE IF NOT EXISTS books (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -16,21 +20,24 @@ CREATE TABLE books (
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
-CREATE TABLE customers (
+-- Create the 'customers' table
+CREATE TABLE IF NOT EXISTS customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
-CREATE TABLE orders (
+-- Create the 'orders' table
+CREATE TABLE IF NOT EXISTS orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
-CREATE TABLE order_details (
+-- Create the 'order_details' table
+CREATE TABLE IF NOT EXISTS order_details (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
