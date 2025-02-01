153e2547cee5f69cc48426bb38b5a9f7966ee96b
Kiruthims <Kiruthimiss@proton.me>
2025-02-01 12:19:59 -0800

    customers and double spaces

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 INSERT INTO customer (customer_name, email, address)
 VALUES
-("Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
-("Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
-("Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.");
+('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
